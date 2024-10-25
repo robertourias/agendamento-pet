21c41d3c886b850314ead94873a14bdfa2d91f31
Roberto Nicoletti <roberto.urias@gmail.com>
2024-10-24 22:06:47 -0300

    configurando comandos e webpack

---
+++ src/main.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// Configuração do dayjs
+import "./libs/days"
+
+// CSS
+import "./styles/global.css"
+
+
+// JS
+// import "./modules/form/submit.js"
+// import "./modules/form/date-change.js"
+// import "./modules/schedules/cancel.js"
+
+import "./modules/page-load.js"
