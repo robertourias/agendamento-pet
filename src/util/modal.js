60ae7507923c616895f8c8a7a564ced546ea1fa2
Roberto Nicoletti <roberto.urias@gmail.com>
2024-11-01 16:34:48 -0300

    fix modal position

--- src/util/modal.js
+++ src/util/modal.js
@@ -1,9 +1,19 @@
+import dayjs from "dayjs";
+
 export function openModal() {
   document.querySelector('body').classList.add('active-modal');  
 }
 
 export function closeModal() {  
   document.querySelector('body').classList.remove('active-modal');
+  document.getElementById('id-field').value = "";
+  document.getElementById('tutor-field').value = "";
+  document.getElementById('pet-field').value = "";
+  document.getElementById('phone-field').value = "";
+  document.getElementById('service-field').value = "";
+  document.getElementById('date-field').value = dayjs(new Date()).format('YYYY-MM-DD');
+  document.getElementById('hour-field').value = "-1";
+  
 }
 
 
